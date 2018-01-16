7b87645162b70a1e749bc71c18f2f79ee69e4445
Fabio Rezende <fabio.rezende@gmail.com>
2018-01-15 22:50:45 -0300

    Initial commit

---
+++ CsvCompare/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CsvCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            this.btnFile1 = new System.Windows.Forms.Button();
+            this.btnFile2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFile1 = new System.Windows.Forms.TextBox();
+            this.txtFile2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Location = new System.Drawing.Point(25, 92);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.Size = new System.Drawing.Size(534, 198);
+            this.dgvFiles.TabIndex = 0;
+            // 
+            // btnFile1
+            // 
+            this.btnFile1.Location = new System.Drawing.Point(210, 51);
+            this.btnFile1.Name = "btnFile1";
+            this.btnFile1.Size = new System.Drawing.Size(75, 23);
+            this.btnFile1.TabIndex = 1;
+            this.btnFile1.Text = "Arquivo 1";
+            this.btnFile1.UseVisualStyleBackColor = true;
+            this.btnFile1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFile2
+            // 
+            this.btnFile2.Location = new System.Drawing.Point(484, 51);
+            this.btnFile2.Name = "btnFile2";
+            this.btnFile2.Size = new System.Drawing.Size(75, 23);
+            this.btnFile2.TabIndex = 2;
+            this.btnFile2.Text = "Arquivo 2";
+            this.btnFile2.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // txtFile1
+            // 
+            this.txtFile1.Location = new System.Drawing.Point(25, 53);
+            this.txtFile1.Name = "txtFile1";
+            this.txtFile1.Size = new System.Drawing.Size(179, 20);
+            this.txtFile1.TabIndex = 3;
+            // 
+            // txtFile2
+            // 
+            this.txtFile2.Location = new System.Drawing.Point(311, 51);
+            this.txtFile2.Name = "txtFile2";
+            this.txtFile2.Size = new System.Drawing.Size(167, 20);
+            this.txtFile2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 474);
+            this.Controls.Add(this.txtFile2);
+            this.Controls.Add(this.txtFile1);
+            this.Controls.Add(this.btnFile2);
+            this.Controls.Add(this.btnFile1);
+            this.Controls.Add(this.dgvFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.Button btnFile1;
+        private System.Windows.Forms.Button btnFile2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox txtFile1;
+        private System.Windows.Forms.TextBox txtFile2;
+    }
+}
+
---
+++ CsvCompare/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvCompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Criando um DataTable
+            DataTable dt = new DataTable();
+
+            //Lendo Todas as linhas do arquivo CSV
+            string[] Linha = System.IO.File.ReadAllLines(@"C:\Projetos GIT\MeuCSV.csv");
+
+            //Neste For, vamos percorrer todas as linhas que foram lidas do arquivo CSV
+            for (Int32 i = 0; i < Linha.Length; i++)
+            {
+                //Aqui Estamos pegando a linha atual, e separando os campos
+                //Por exemplo, ele vai lendo um texto, e quando achar um ponto e virgula
+                //ele pega o texto e joga na outra posição do array temp, e assim por diante
+                //até chegar no final da linha
+                string[] campos = Linha[i].Split(Convert.ToChar(";"));
+
+                //Um datable precisa de colunas
+                //Como a função acima jogou cada campo em uma posição do array de acordo com
+                //o ponto e virgula, eu estou pegando quantos campos ele achou e criando a mesma
+                //quantidade de colunas no datatable para corresponder a cada campo
+                if (i == 0)
+                {
+                    for (Int32 i2 = 0; i2 < campos.Length; i2++)
+                    {
+                        //Criando uma coluna
+                        DataColumn col = new DataColumn();
+                        //Adicionando a coluna criada ao datatable
+                        dt.Columns.Add(col);
+                    }
+                }
+
+                //Inserindo uma linha(row) no datable (Vai fazer isso para cada linha encontrada
+                //no arquivo CSV
+                dt.Rows.Add(campos);
+            }
+
+            //Depois de montado o datatable, vamos falar para o grid
+            //que a fonte de dados para ele exibir, será o datatable que 
+            //a gente acabou de criar
+            dgvFiles.DataSource = dt;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            txtFile1.Text = openFileDialog1.FileName;
+            BindDataCSV(txtFile1.Text);
+        }
+
+        private void BindDataCSV(string filePath)
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            if (lines.Length > 0)
+            {
+                //primeira linha cria header
+                string firstLine = lines[0];
+                string[] headerLabels = firstLine.Split(';');
+                foreach (string headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+
+                for (int r = 1; r < lines.Length; r++)
+                {
+                    string[] dataWords = lines[r].Split(';');
+                    DataRow dr = dt.NewRow();
+                    int columnIndex = 0;
+
+                    foreach(string headerWord in headerLabels)
+                    {
+                        dr[headerWord] = dataWords[columnIndex++];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            if(dt.Rows.Count>0)
+            {
+                dgvFiles.DataSource = dt;
+            }
+        }
+    }
+}
