92df995cb99a94a2aaee670714afbfaff7206a08
fabio rezende <fabio.rezende@gmail.com>
2018-01-19 18:57:06 -0300

    Alteracao design efetuada pelo Leandro

--- CsvCompare/Form1.Designer.cs
+++ CsvCompare/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnCompare = new System.Windows.Forms.Button();
             this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.SuspendLayout();
@@ -137,12 +139,38 @@
             this.pictureBoxLogo.TabIndex = 7;
             this.pictureBoxLogo.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(470, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CSV SE";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(341, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "CSV SE";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(758, 454);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBoxLogo);
             this.Controls.Add(this.btnCompare);
             this.Controls.Add(this.label1);
@@ -175,6 +203,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCompare;
         private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
